ea5471bd85b53dedd0df9ec74f66938353ed6c1f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2024-05-13 20:39:12 +0600

    [DONE] filter functionality done

--- dpt-frontend-task-react.js/src/components/global/Dropdown.jsx
+++ dpt-frontend-task-react.js/src/components/global/Dropdown.jsx
@@ -1,30 +1,32 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const Dropdown = ({ options, onSelect }) => {
- const [selectedOption, setSelectedOption] = useState("default");
+const Dropdown = ({ options, onSelect, name }) => {
+  const [selectedOption, setSelectedOption] = useState("default");
 
- const handleSelectChange = (event) => {
-  const value = event.target.value;
-  setSelectedOption(value);
-  onSelect(value);
- };
+  const handleSelectChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onSelect(value);
+  };
 
- return (
-  <select
-   value={selectedOption}
-   onChange={handleSelectChange}
-   className='border rounded p-2 w-full'
-   name=''>
-   <option value='' disabled>
-    Select an option
-   </option>
-   {options?.map((option, index) => (
-    <option key={index} value={option.value}>
-     {option.label}
-    </option>
-   ))}
-  </select>
- );
+  return (
+    <select
+      value={selectedOption}
+      onChange={handleSelectChange}
+      className='border rounded p-2 w-full outline-none border-gray-400'
+      name={name}
+    >
+      <option value='' disabled>
+        Select an option
+      </option>
+      {options?.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default Dropdown;
--- dpt-frontend-task-react.js/src/constants/filterOptions.js
+++ dpt-frontend-task-react.js/src/constants/filterOptions.js
@@ -1,44 +1,44 @@
 const options1 = {
- name: "airport1",
- data: [
-  { value: "LHR", label: "LHR" },
-  { value: "CDG", label: "CDG" },
-  { value: "DAC", label: "DAC" },
- ],
+    name: "departureRoute",
+    data: [
+        { value: "LHR", label: "LHR" },
+        { value: "CDG", label: "CDG" },
+        { value: "DAC", label: "DAC" },
+    ],
 };
 const options2 = {
- name: "airport2",
- data: [
-  { value: "CDG", label: "CDG" },
-  { value: "LHR", label: "IST" },
-  { value: "DAC", label: "FCO" },
- ],
+    name: "arrivalRoute",
+    data: [
+        { value: "CDG", label: "CDG" },
+        { value: "IST", label: "IST" },
+        { value: "FCO", label: "FCO" },
+    ],
 };
 const options3 = [{ value: "day-", label: "Day -" }];
 const options4 = [{ value: "day+", label: "Day +" }];
 const options5 = {
- name: "time",
- data: [
-  { value: "At", label: "Any time" },
-  { value: "Win", label: "Winter" },
-  { value: "Sum", label: "Summer" },
- ],
+    name: "time",
+    data: [
+        { value: "At", label: "Any time" },
+        { value: "Win", label: "Winter" },
+        { value: "Sum", label: "Summer" },
+    ],
 };
 const options6 = {
- name: "additional",
- data: [
-  { value: "ADT", label: "ADT" },
-  { value: "CLD", label: "CLD" },
-  { value: "KID", label: "KID" },
- ],
+    name: "additional",
+    data: [
+        { value: "ADT", label: "ADT" },
+        { value: "CLD", label: "CLD" },
+        { value: "KID", label: "KID" },
+    ],
 };
 const options7 = {
- name: "options7",
- data: [
-  { value: "1", label: "1" },
-  { value: "2", label: "2" },
-  { value: "3", label: "3" },
- ],
+    name: "options7",
+    data: [
+        { value: "1", label: "1" },
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+    ],
 };
 
 export { options1, options2, options3, options4, options5, options6, options7 };
